3e04030e001d726254fdbdd8a66fd7ca4b3daeed
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-04 12:25:05 +0200

    Add Laravel Jetstream and fix config

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,24 @@
 <?php
 
-declare(strict_types=1);
-
+use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
-use Inertia\Response;
+use Inertia\Inertia;
 
-Route::get("/", fn(): Response => inertia("Welcome"));
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified',
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return Inertia::render('Dashboard');
+    })->name('dashboard');
+});
