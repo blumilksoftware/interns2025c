5549e7e54ff5366cfee9f554df5a1a5f33b0fd7e
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-05 10:51:48 +0200

    Add pet shelter migrations

---
+++ database/migrations/2025_08_05_075231_pet_shelter_address.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('pet_shelter_addresses', function (Blueprint $table) {
+            $table->id();
+            $table->string('address')->nullable();
+            $table->string('city')->nullable();
+            $table->string('postal_code')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('pet_shelter_addresses');
+    }
+};
