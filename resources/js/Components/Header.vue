2873ff054e92f62a16bdc932258f20170fb44e73
TomaszRebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2025-08-06 11:05:39 +0200

    Apply PR suggestions

--- resources/js/Components/Header.vue
+++ resources/js/Components/Header.vue
@@ -18,8 +18,8 @@ const mobileMenuOpen = ref(false)
     <nav class="mx-auto flex max-w-7xl items-center justify-between p-6 lg:px-8" aria-label="Global">
       <div class="flex lg:flex-1">
         <a href="#" class="-m-1.5 p-1.5">
-          <span class="sr-only">Interns2025c</span>
-          <img class="h-8 w-auto" src="/Images/cat-dog.png" alt="App logo">
+          <span class="sr-only">Go to homepage</span>
+          <img class="h-8 w-auto" src="/Images/cat-dog.png" alt="Interns2025c app logo">
         </a>
       </div>
       <div class="flex lg:hidden">
