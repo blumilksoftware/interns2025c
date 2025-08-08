95c3ddd41707e5527b828260da26ba121992187a
DenysZachyniaiev <denys.zachyniaiev@studenci.collegiumwitelona.pl>
2025-08-08 12:54:12 +0200

    Fix linter

--- routes/web.php
+++ routes/web.php
@@ -2,8 +2,9 @@
 
 declare(strict_types=1);
 
+use App\Http\Controllers\TagController;
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
-use App\Http\Controllers\TagController;
+
 Route::get("/", fn(): Response => inertia("LandingPage"));
-Route::resource('tags', TagController::class)->only(['store', 'update', 'destroy']);
+Route::resource("tags", TagController::class)->only(["store", "update", "destroy"]);
