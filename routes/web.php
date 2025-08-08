f0dcb1b7653692d80b55c9a59f9295298a791028
DenysZachyniaiev <denys.zachyniaiev@studenci.collegiumwitelona.pl>
2025-08-08 12:28:31 +0200

    Fix test

--- routes/web.php
+++ routes/web.php
@@ -4,5 +4,7 @@ declare(strict_types=1);
 
 use Illuminate\Support\Facades\Route;
 use Inertia\Response;
+use App\Http\Controllers\TagController;
 
 Route::get("/", fn(): Response => inertia("Welcome"));
+Route::apiResource('tags', TagController::class)->only(['store', 'update', 'destroy']);
