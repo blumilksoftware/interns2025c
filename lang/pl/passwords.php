51e9feb98a0f6b00e7463899e07c3bd319aef891
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-11 14:45:59 +0200

    Move translations to passwords.php

---
+++ lang/pl/passwords.php
@@ -0,0 +1,15 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    "reset" => "Twoje hasło zostało zresetowane.",
+    "sent" => "Wysłaliśmy link do resetowania hasła na Twój adres e-mail. Jeżeli nie widzisz wiadomości sprawdź folder SPAM.",
+    "throttled" => "Proszę zaczekać przed ponowną próbą.",
+    "token" => "Ten token resetowania hasła jest nieprawidłowy.",
+    "user" => "Nie możemy znaleźć użytkownika z tym adresem e-mail.",
+    "passwords" => [
+        "user" => "Użytkownik z tym adresem e-mail nie istnieje.",
+        "token" => "Token resetowania hasła jest nieprawidłowy lub wygasł.",
+    ],
+];
