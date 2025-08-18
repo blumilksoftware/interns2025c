3451196fe5e732c4f24b59c0b8adb7e01cbc6ea9
TomaszRebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2025-08-18 09:45:06 +0200

    fix missing title on landing page

--- lang/pl/titles.php
+++ lang/pl/titles.php
@@ -3,7 +3,7 @@
 declare(strict_types=1);
 
 return [
-    "dashboard" => "Panel główny - interns2025c",
-    "landingPage" => "Strona główna - interns2025c",
-    "adminPanel" => "Panel administracyjny - interns2025c",
+    "dashboard" => "Panel główny",
+    "landingPage" => "Strona główna",
+    "adminPanel" => "Panel administracyjny",
 ];
