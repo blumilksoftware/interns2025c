7a2064c5dec08899f2a6a3d83cbc668649423e73
DenysZachyniaiev <147748545+DenysZachyniaiev@users.noreply.github.com>
2025-08-19 10:28:52 +0200

    #19 - Create CRUD for tags (#28)
    * Create crud for Tag system
    
    * Fix linter erros
    
    * Remove empty lines
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * Use TagRequest properly
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    
    * Update
    
    * Fixing linter
    
    * Fix test
    
    * Fix linter
    
    * workflow
    
    * workflow
    
    * workflow
    
    * declaring return types
    
    * fix resource usage in store in controller
    
    * implement suggestions
    
    * Fix and update tag seeding logic
    
    * Update Tag Controller, add Tag Policy
    
    * Add tests, refactor TagResource
    
    * Change from TagController to match PetController style, update tests
    
    * Apply PR suggestions
    
    * Apply PR suggestions
    
    ---------
    
    Co-authored-by: Kamil Piech <kamil.piech@blumilk.pl>
    Co-authored-by: Aleksandra Kozubal <aleksandra.kozubal@studenci.collegiumwitelona.pl>
    Co-authored-by: Krzysztof Kozyra <87642957+krzysztofkozyra021@users.noreply.github.com>
    Co-authored-by: krzysztofkozyra021 <cysiuu021@protonmail.com>

---
+++ app/Http/Controllers/TagController.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\TagRequest;
+use App\Models\Tag;
+use Illuminate\Http\RedirectResponse;
+
+class TagController extends Controller
+{
+    public function store(TagRequest $request): RedirectResponse
+    {
+        $this->authorize("store", Tag::class);
+
+        Tag::query()->create($request->validated());
+
+        return redirect("/admin")
+            ->with("success", "Tag created successfully.");
+    }
+
+    public function update(TagRequest $request, Tag $tag): RedirectResponse
+    {
+        $this->authorize("update", $tag);
+
+        $tag->update($request->validated());
+
+        return redirect("/admin")
+            ->with("success", "Tag updated successfully.");
+    }
+
+    public function destroy(Tag $tag): RedirectResponse
+    {
+        $this->authorize("delete", $tag);
+
+        $tag->delete();
+
+        return redirect("/admin")
+            ->with("success", "Tag deleted successfully.");
+    }
+}
