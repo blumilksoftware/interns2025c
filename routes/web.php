855684bf1169caaa9e592a925c7406d0cb206e11
TomaszRebizant <tomasz.rebizant@studenci.collegiumwitelona.pl>
2025-08-19 11:11:17 +0200

    lint fixes

--- routes/web.php
+++ routes/web.php
@@ -5,16 +5,17 @@ declare(strict_types=1);
 use App\Http\Controllers\PetController;
 use App\Http\Controllers\PetShelterAddressController;
 use App\Http\Controllers\PetShelterController;
+use App\Http\Controllers\TagController;
 use Illuminate\Foundation\Application;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Inertia\Response;
 
-Route::get("/", fn() => Inertia::render("LandingPage/LandingPage", [
+Route::get("/", fn() => Inertia::render("LandingPage", [
     "canLogin" => Route::has("login"),
     "canRegister" => Route::has("register"),
     "laravelVersion" => Application::VERSION,
     "phpVersion" => PHP_VERSION,
-    "title" => __("titles.landingPage"),
 ]));
 
 Route::middleware([
@@ -22,19 +23,11 @@ Route::middleware([
     config("jetstream.auth_session"),
     "verified",
 ])->group(function (): void {
-    Route::get("/dashboard", fn() => Inertia::render("Dashboard/Dashboard", [
-        "title" => __("titles.dashboard"),
-    ]))->name("dashboard");
-    Route::get("/admin", fn() => Inertia::render("AdminPanel/AdminPanel", [
-        "title" => __("titles.adminPanel"),
-    ]))->name("admin");
+    Route::get("/dashboard", fn() => Inertia::render("LandingPage"))->name("dashboard");
 });
 
-Route::resource("pet-shelter-addresses", PetShelterAddressController::class)
-    ->only(["store", "update", "destroy"]);
-
-Route::resource("pet-shelters", PetShelterController::class)
-    ->only(["index", "store", "update", "destroy"]);
-
-Route::resource("pets", PetController::class)
-    ->except(["create", "edit"]);
+Route::get("/admin", fn(): Response => inertia("AdminPanel/AdminPanel"));
+Route::resource("pet-shelter-addresses", PetShelterAddressController::class)->only("store", "update", "destroy");
+Route::resource("pet-shelters", PetShelterController::class)->only("index", "store", "update", "destroy");
+Route::resource("pets", PetController::class)->except(["create", "edit"]);
+Route::resource("tags", TagController::class)->only(["store", "update", "destroy"]);
