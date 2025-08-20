be7672a804b4c1fc61b80d45de836a4123363556
DenysZachyniaiev <denys.zachyniaiev@studenci.collegiumwitelona.pl>
2025-08-20 11:17:11 +0200

    crud

--- app/Http/Requests/PreferenceRequest.php
+++ app/Http/Requests/PreferenceRequest.php
@@ -5,22 +5,17 @@ declare(strict_types=1);
 namespace App\Http\Requests;
 
 use Illuminate\Foundation\Http\FormRequest;
-use Illuminate\Validation\Rule;
 
 class PreferenceRequest extends FormRequest
 {
     public function rules(): array
     {
-        $preferenceId = $this->route("preference")?->id;
-
         return [
             "name" => [
                 "required",
                 "string",
                 "max:255",
-                Rule::unique("preferences")
-                    ->ignore($preferenceId)
-                    ->where(fn($query) => $query->where("user_id", $this->user()->id)),
+                "unique:preferences,name,NULL,id,user_id," . $this->user()->id,
             ],
         ];
     }
