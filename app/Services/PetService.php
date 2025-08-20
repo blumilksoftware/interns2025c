17b43644f367be8fd25dfd12616aca9e45991b9d
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-20 14:49:34 +0200

    Update PetService add pet to db method

--- app/Services/PetService.php
+++ app/Services/PetService.php
@@ -6,9 +6,11 @@ namespace App\Services;
 
 use App\Models\Pet;
 use App\Models\PetShelter;
-use App\Utils\PetEnumMapper;
+use App\Models\Tag;
 use App\Utils\UrlFormatHelper;
+use Carbon\Carbon;
 use Illuminate\Support\Facades\Log;
+use Illuminate\Support\Str;
 
 class PetService
 {
@@ -25,23 +27,40 @@ class PetService
             return;
         }
 
+        $tags = Str::of($petData["animals"][0]["behavioral_notes"] ?? "")
+            ->explode(" ")
+            ->map(fn($tag) => trim($tag))
+            ->filter()
+            ->unique()
+            ->toArray();
+
+        foreach ($tags as $tag) {
+            Tag::query()->firstOrCreate(["name" => $tag]);
+            Log::info("Tag created: $tag");
+        }
+
         foreach ($petData["animals"] ?? [] as $animal) {
-            Pet::query()->create([
+            $identifyingAttributes = [
                 "name" => $animal["name"],
-                "species" => PetEnumMapper::mapSpecies($animal["species"]),
-                "sex" => PetEnumMapper::mapSex($animal["sex"]),
+                "species" => $animal["species"],
+                "behavioral_notes" => $animal["behavioral_notes"] ?? null,
+                "shelter_id" => $shelter->id,
+                "sex" => $animal["sex"],
                 "breed" => $animal["breed"] ?? null,
+                "weight" => $animal["weight"] ?? null,
+                "chip_number" => $animal["chip_number"] ?? null,
+                "found_location" => $animal["found_location"] ?? null,
+            ];
+
+            $attributes = [
                 "age" => $animal["age"] ?? null,
                 "size" => $animal["size"] ?? null,
-                "weight" => $animal["weight"] ?? null,
                 "color" => $animal["color"] ?? null,
                 "sterilized" => $animal["sterilized"] ?? null,
                 "description" => $animal["description"],
-                "health_status" => PetEnumMapper::mapHealthStatus($animal["health_status"] ?? null),
+                "health_status" => $animal["health_status"] ?? null,
                 "current_treatment" => $animal["current_treatment"] ?? null,
                 "vaccinated" => $animal["vaccinated"] ?? null,
-                "has_chip" => $animal["has_chip"] ?? null,
-                "chip_number" => $animal["chip_number"] ?? null,
                 "dewormed" => $animal["dewormed"] ?? null,
                 "deflea_treated" => $animal["deflea_treated"] ?? null,
                 "medical_tests" => $animal["medical_tests"] ?? null,
@@ -51,13 +70,13 @@ class PetService
                 "attitude_to_cats" => $animal["attitude_to_cats"] ?? null,
                 "attitude_to_children" => $animal["attitude_to_children"] ?? null,
                 "activity_level" => $animal["activity_level"] ?? null,
-                "behavioral_notes" => $animal["behavioral_notes"] ?? null,
-                "admission_date" => $animal["admission_date"] ?? null,
-                "found_location" => $animal["found_location"] ?? null,
                 "adoption_status" => $animal["adoption_status"] ?? null,
-                "shelter_id" => $shelter->id,
+                "admission_date" => isset($animal["admission_date"]) ? Carbon::parse($animal["admission_date"]) : null,
+                "has_chip" => $animal["has_chip"] ?? null,
                 "adoption_url" => $adoptionUrl,
-            ]);
+            ];
+
+            Pet::query()->updateOrCreate($identifyingAttributes, $attributes);
         }
     }
 }
