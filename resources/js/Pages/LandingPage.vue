bb15e0e4be9c1a0fa90079ae1c7447c2911b6b7f
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-08-25 23:10:30 +0200

    Fix JS linter error

--- resources/js/Pages/LandingPage.vue
+++ resources/js/Pages/LandingPage.vue
@@ -5,7 +5,7 @@ import BackgroundGradient from '@/Components/BackgroundGradient.vue'
 import ButtonSection from './LandingPage/ButtonSection.vue'
 import Footer from '@/Components/Footer.vue'
 import { useI18n } from 'vue-i18n'
-import {Head} from "@inertiajs/vue3";
+import { Head } from '@inertiajs/vue3'
 const { t } = useI18n()
 
 const title = t('title.userProfile')
