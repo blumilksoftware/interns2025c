33528c1239b71e9f84c6d829cc37c8e9889312b3
krzysztofkozyra021 <cysiuu021@protonmail.com>
2025-09-01 09:55:44 +0200

    Add option button, move buttons to folder

---
+++ resources/js/Components/Buttons/OptionButton.vue
@@ -0,0 +1,23 @@
+<template>
+  <button
+    type="button"
+    class="inline-flex items-center gap-x-3 rounded-md px-2.5 py-1.5 text-sm font-semibold text-white shadow-xs focus-visible:outline-2 focus-visible:outline-offset-2"
+  >
+    <component :is="icon" class="-ml-0.5 size-5" aria-hidden="true" />
+    {{ text }}
+  </button>
+</template>
+
+<script setup>
+const props = defineProps({
+  text: {
+    type: String,
+    required: true,
+  },
+  icon: {
+    type: Object,
+    required: true,
+  },
+})
+
+</script>
