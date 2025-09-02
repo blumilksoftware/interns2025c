083b99561a9c8907a92c63078e714109949b97cb
DenysZachyniaiev <denys.zachyniaiev@studenci.collegiumwitelona.pl>
2025-09-02 13:58:40 +0200

    implement suggestions

--- app/Http/Resources/PetMatchResource.php
+++ app/Http/Resources/PetMatchResource.php
@@ -4,11 +4,12 @@ declare(strict_types=1);
 
 namespace App\Http\Resources;
 
+use Illuminate\Http\Request;
 use Illuminate\Http\Resources\Json\JsonResource;
 
 class PetMatchResource extends JsonResource
 {
-    public function toArray($request): array
+    public function toArray(Request $request): array
     {
         return [
             "pet" => new PetIndexResource($this->resource["pet"]),
